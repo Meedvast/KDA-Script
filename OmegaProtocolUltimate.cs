7e448c6ac273df43a7a651c184087a0a42449c89
Meedvast <91186004+Meedvast@users.noreply.github.com>
2025-06-27 21:50:10 +0800

    fix:FFCS 7.2

--- OmegaProtocolUltimate.cs
+++ OmegaProtocolUltimate.cs
@@ -27,7 +27,7 @@ using FFXIVClientStructs.FFXIV.Client.Game.Object;
 namespace MyScriptNamespace
 {
     
-    [ScriptType(name: "绝欧精装豪华版", territorys: [1122],guid: "e0bfb4db-0d38-909f-5088-b23f09b7585e", version:"0.0.0.5", author:"Karlin",note: noteStr)]
+    [ScriptType(name: "绝欧精装豪华版", territorys: [1122],guid: "e0bfb4db-0d38-909f-5088-b23f09b7585e", version:"0.0.0.6", author:"Karlin",note: noteStr)]
     public class OmegaProtocolUltimate
     {
         const string noteStr =
@@ -2296,7 +2296,7 @@ namespace MyScriptNamespace
                     FFXIVClientStructs.FFXIV.Client.UI.Agent.AgentMap* ptr = FFXIVClientStructs.FFXIV.Client.UI.Agent.AgentMap.Instance();
                     if (ptr is not null)
                     {
-                        isMoving = ptr->IsPlayerMoving == 1;
+                        isMoving = ptr->IsPlayerMoving;
                     }
                 }
                 return isMoving;
